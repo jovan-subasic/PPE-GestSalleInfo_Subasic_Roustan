22bf406877843d128483262c060c2f1aa08e2e72
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-11-30 17:36:02 +0100

    Update SalleInfoManager.php

--- modele/SalleInfoManager.php
+++ modele/SalleInfoManager.php
@@ -5,21 +5,21 @@ require_once("modele/SalleInfo.php");
 
 class SalleInfoManager extends Manager
 {
-    public function get($id) //instancie un objet secteur
+    public function get($id) //instancie un objet salle
     {
         $id = (int) $id;
-        $q = $this->getPDO()->query('SELECT id, nom FROM secteur WHERE id = '.$id);
+        $q = $this->getPDO()->query('SELECT id, nSalle, nomSalle, nbPoste, indIP, room_name, sort_key, description, capacity  FROM salle inner join mrbs_room on mrbs_room.id = salle.id ');
         $donnees = $q->fetch(PDO::FETCH_ASSOC);
-        return new Secteur($donnees['id'], $donnees['nom']);
+        return new SalleInfo ($donnees['id'], $donnees['nSalle'], $donnees['nomSalle'], $donnees['nbPoste'], $donnees['indIP'] );
     }
   
-    public function getList() //instancie une collection d'objets secteurs
+    public function getList() //instancie une collection d'objets salles
     {
         $salleinfos = [];
-        $q = $this->getPDO()->query('SELECT id, nom FROM secteur ORDER BY nom');
+        $q = $this->getPDO()->query('SELECT id, nSalle, nomSalle, nbPoste, indIP, room_name, sort_key, description, capacity  FROM salle inner join mrbs_room on mrbs_room.id = salle.id ORDER BY nom');
         while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
         {
-            $sallesinfos[$donnees['id']] = new Secteur($donnees['id'], $donnees['nom']);
+            $sallesinfos[$donnees['id']] = new SalleInfo ($donnees['id'], $donnees['nSalle'], $donnees['nomSalle'], $donnees['nbPoste'], $donnees['indIP'] );
         }
         return $salleinfos;
     }
