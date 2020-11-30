f11a2b85df05a2778464abb3efb0962b1315a22e
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-11-30 17:41:31 +0100

    Update afficheSalleInfo.php

--- controleur/afficheSalleInfo.php
+++ controleur/afficheSalleInfo.php
@@ -3,26 +3,5 @@
 require_once("modele/Manager.php");
 require_once("modele/SalleInfo.php");
 
-class SalleInfoManager extends Manager
-{
-    public function get($id) //instancie un objet SalleInfo
-    {
-        $id = (int) $id;
-        $q = $this->getPDO()->query('SELECT id, nom FROM SalleInfo WHERE id = '.$id);
-        $donnees = $q->fetch(PDO::FETCH_ASSOC);
-        return new SalleInfo($donnees['id'], $donnees['nom']);
-    }
-  
-    public function getList() //instancie une collection d'objets SalleInfos
-    {
-        $SalleInfos = [];
-        $q = $this->getPDO()->query('SELECT id, nom FROM SalleInfo ORDER BY nom');
-        while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
-        {
-            $SalleInfos[$donnees['id']] = new SalleInfo($donnees['id'], $donnees['nom']);
-        }
-        return $SalleInfos;
-    }
-    
-}*/
+*/
 ?>
