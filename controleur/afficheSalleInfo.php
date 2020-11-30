f7f8ca14ecdc73bce7408e761e5e1e3e5a6e1e8a
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-11-30 17:38:07 +0100

    Update afficheSalleInfo.php

--- controleur/afficheSalleInfo.php
+++ controleur/afficheSalleInfo.php
@@ -1,27 +1,27 @@
 <?php
 /*
 require_once("modele/Manager.php");
-require_once("modele/Secteur.php");
+require_once("modele/SalleInfo.php");
 
-class SecteurManager extends Manager
+class SalleInfoManager extends Manager
 {
-    public function get($id) //instancie un objet secteur
+    public function get($id) //instancie un objet SalleInfo
     {
         $id = (int) $id;
-        $q = $this->getPDO()->query('SELECT id, nom FROM secteur WHERE id = '.$id);
+        $q = $this->getPDO()->query('SELECT id, nom FROM SalleInfo WHERE id = '.$id);
         $donnees = $q->fetch(PDO::FETCH_ASSOC);
-        return new Secteur($donnees['id'], $donnees['nom']);
+        return new SalleInfo($donnees['id'], $donnees['nom']);
     }
   
-    public function getList() //instancie une collection d'objets secteurs
+    public function getList() //instancie une collection d'objets SalleInfos
     {
-        $secteurs = [];
-        $q = $this->getPDO()->query('SELECT id, nom FROM secteur ORDER BY nom');
+        $SalleInfos = [];
+        $q = $this->getPDO()->query('SELECT id, nom FROM SalleInfo ORDER BY nom');
         while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
         {
-            $secteurs[$donnees['id']] = new Secteur($donnees['id'], $donnees['nom']);
+            $SalleInfos[$donnees['id']] = new SalleInfo($donnees['id'], $donnees['nom']);
         }
-        return $secteurs;
+        return $SalleInfos;
     }
     
 }*/
