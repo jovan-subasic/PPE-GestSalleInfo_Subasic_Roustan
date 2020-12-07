645313259d86e0f6b36d26847507641f7c21611d
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-12-07 17:08:17 +0100

    Update accueil.php

--- controleur/accueil.php
+++ controleur/accueil.php
@@ -4,7 +4,7 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
 }
 
 
-$titre = "Accueil";
+$titre = "Liste des bateaux";
 include "$racine/vue/header.php";
 include "$racine/vue/vueAccueil.php";
 include "$racine/vue/footer.php";
