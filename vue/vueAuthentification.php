cdc4debab8c07a09428f3a85e9cb99669dfd3e76
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-12-07 17:55:34 +0100

    Create vueAuthentification.php

---
+++ vue/vueAuthentification.php
@@ -0,0 +1,14 @@
+<h1>Connexion</h1>
+<form action="./?action=connexion" method="POST">
+
+    <input type="text" name="pseudoU" placeholder="nom d'utilisateur" /><br />
+    <input type="password" name="mdpU" placeholder="Mot de passe"  /><br />
+    <input type="submit" />
+
+</form>
+
+<hr>
+Utilisateur: <br />
+login : admin<br />
+mot de passe : sio
+
