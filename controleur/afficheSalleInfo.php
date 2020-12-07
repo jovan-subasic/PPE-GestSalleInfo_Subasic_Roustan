9051b72b6f5fc1362bb1ecc0718762a8c184bd08
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-12-07 17:14:13 +0100

    Update afficheSalleInfo.php

--- controleur/afficheSalleInfo.php
+++ controleur/afficheSalleInfo.php
@@ -1,9 +1,13 @@
 <?php
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine = "..";
+}
+include_once "$racine/modele/bd.liaison.inc.php";
 
-require_once("racine/modele/SalleInfoManager.php");
+//require_once("racine/modele/SalleInfoManager.php");
 $titre= "Liste des salles";
-$salleinfoManager= new SalleInfoManager();
-$salleinfos=$salleinfoManager->getList();
+//$salleinfoManager= new SalleInfoManager();
+//$salleinfos=$salleinfoManager->getList();
 
 include "racine/vue/header.php";
 include "racine/vue/vueAfficheSalleInfo.php";
