b6a27f23f3ee55c1fd351fbe40832039417d3abf
tanguy-r69 <75259833+tanguy-r69@users.noreply.github.com>
2020-12-14 15:37:42 +0100

    Update vueAfficheSalleInfo.php

--- vue/vueAfficheSalleInfo.php
+++ vue/vueAfficheSalleInfo.php
@@ -1,35 +1,41 @@
 <?php
-  //var_dump($titre);
+  //var_dump($salleinfos);
 ?>
   <h1><?= $titre ?></h1>
   <table class='SalleInfo'>
 			
 		<tr>
-			<th>Id</th>
-			<th>NSalle</th>
+			<th>ID</th>
+			<th>NumSalle</th>
 			<th>NomSalle</th>
-			<th>NbPoste</th>
+			<th>Nombre de Poste</th>
 			<th>IndIP</th>
 			<th>Room_name</th>
-      <th>Sort_key</th>
-      <th>Description</th>
-      <th>Capacity</th>
+      		<th>Sort_key</th>
+      		<th>Description</th>
+     		<th>Capacity</th>
 		</tr>
 	
 		<?php
 			foreach ($salleinfos as $salleinfo)
 			{
 		?> 
-		<tr>
-			<td><?= $instrument->getId() ?></td>
-			<td><?= $instrument->getNSalle() ?></td>
-			<td><?= $instrument->getNomSalle() ?></td>
-			<td><?= $instrument->getNbPoste() ?></td>
-			<td><?= $instrument->getIndIP() ?></td>
-			
 		
-		<?php } ?>
-		</tr>	
+
+			<tr>
+				<td><?= $salleinfo->getId() ?></td>
+				<td><?= $salleinfo->getNSalle() ?></td>
+				<td><?= $salleinfo->getNomSalle() ?></td>
+				<td><?= $salleinfo->getNbPoste() ?></td>
+				<td><?= $salleinfo->getIndIP() ?></td>
+				<td><?= $salleinfo->getRoom_name()?></td>
+				<td><?= $salleinfo->getSort_key() ?></td>
+				<td><?= $salleinfo->getDescription() ?></td>
+				<td><?= $salleinfo->getCapacity() ?></td>
+				
+			
+			<?php } ?>
+			</tr>	
 								
 			
 	</table>
