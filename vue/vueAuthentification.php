332082c63f4812727c4ed14b433201a5dd1b97bf
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-12-14 17:53:39 +0100

    Update vueAuthentification.php

--- vue/vueAuthentification.php
+++ vue/vueAuthentification.php
@@ -1,24 +1,4 @@
-<?php
-   session_start();
-   @$name=$_POST["name"];
-   @$password=md5($_POST["password"]);
-   @$valider=$_POST["valider"];
-   $erreur="";
-   if(isset($valider)){
-      include("modele/Manager.php");
-      $sel=$pdo->prepare("select * from mrbs_users where name=? and password=? limit 1");
-      $sel->execute(array($name,$password));
-      $tab=$sel->fetchAll();
-      if(count($tab)>0){
-         $_SESSION["prenomNom"]=ucfirst(strtolower($tab[0]["prenom"])).
-         " ".strtoupper($tab[0]["nom"]);
-         $_SESSION["autoriser"]="oui";
-         header("location:session.php");
-      }
-      else
-         $erreur="Mauvais login ou mot de passe!";
-   }
-?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -52,13 +32,13 @@
          }
       </style>
    </head>
-   <body onLoad="document.fo.name.focus()">
+  
       <h1>Authentification</h1>
-      <div class="erreur"><?php echo $erreur ?></div>
-      <form name="fo" method="post" action="">
-         <input type="text" name="name" placeholder="nom d'utilisateur" /><br />
-         <input type="passwordword" name="password" placeholder="Mot de passe" /><br />
-         <input type="submit" name="valider" value="S'authentifier" />
+
+      <form action="./?action=connexion" method="POST">
+        <input type="text" name="pseudoU" placeholder="nom d'utilisateur" /><br />
+        <input type="password" name="mdpU" placeholder="Mot de passe"  /><br />
+        <input type="submit" />
       </form>
-   </body>
+
 </html>
