555f1dc716e420560c1af480c8cfc0850c0ec0e6
tanguy-r69 <75259833+tanguy-r69@users.noreply.github.com>
2020-12-14 14:38:46 +0100

    Update afficheSalleInfo.php

--- controleur/afficheSalleInfo.php
+++ controleur/afficheSalleInfo.php
@@ -2,20 +2,14 @@
 if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     $racine = "..";
 }
-include_once "$racine/modele/bd.authentification.inc.php";
-include_once "$racine/modele/bd.liaison.inc.php";
 
-//require_once("racine/modele/SalleInfoManager.php");
+
+require_once("$racine/modele/SalleInfoManager.php");
 $titre= "Liste des salles";
-//$salleinfoManager= new SalleInfoManager();
+$salleinfoManager= new SalleInfoManager();
 $salleinfos=$salleinfoManager->getList();
 
 include "$racine/vue/header.php";
-//if (isLoggedOn()){
 include "$racine/vue/vueAfficheSalleInfo.php";
 include "$racine/vue/footer.php";
-//}
-//else{
-//    echo ("Non Connecté");
-//}
 ?>
