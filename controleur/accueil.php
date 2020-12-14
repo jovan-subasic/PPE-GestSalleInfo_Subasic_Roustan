a8064a6fcd7f9ef5fcf8215e387961e1c3f95dbc
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2020-12-14 14:43:17 +0100

    Update accueil.php

--- controleur/accueil.php
+++ controleur/accueil.php
@@ -3,22 +3,12 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
 
-include_once "$racine/modele/bd.authentification.inc.php";
+
 
 
 $titre = "Accueil";
-include "$racine/vue/header.php";
-if(isLoggedOn()){
-    include_once "$racine/modele/bd.musique.inc.php";
-    include_once "$racine/modele/bd.responsables.inc.php";
-
-    ?><button onclick="window.location.href = './?action=deconnexion';">Déconnexion</button><?php
-}
-else{
-    //include "$racine/vue/header.php";
-    $deco = logout();
+    include "$racine/vue/header.php";
     include "$racine/vue/vueAccueil.php";
-    echo ("Non Connecté");
-    //include "$racine/vue/footer.php";
-}
+    include "$racine/vue/footer.php";
+
 ?>
