14f8f68959904004c2212f965bc124788a1d4044
jovan-subasic <60462303+jovan-subasic@users.noreply.github.com>
2021-04-27 17:39:49 +0200

    Update connexion.php

--- controleur/connexion.php
+++ controleur/connexion.php
@@ -2,10 +2,23 @@
 if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
+require_once("modele/Manager.php");
 
-if (isset($_POST["pseudo"]) && isset($_POST["mdp"])){
+if (isset($_POST["pseudo"]) && isset($_POST["mdp"]))
+{
     $pseudo=$_POST["pseudo"];
     $mdp=$_POST["mdp"];
+
+    $cnx = dbConnect();
+    $req = $cnx->prepare("select * from mrbs_users where name = :name");
+    $req->bindValue(':name', $pseudo, PDO::PARAM_STR);
+    $req->execute();
+    $resultat = $req->fetch(PDO::FETCH_ASSOC);
+
+
+    echo '<pre>';
+    print_r($resultat);
+    echo '</pre>';
 }
 else
 {
@@ -13,6 +26,9 @@ else
     $mdp="";
 }
 
+// $hash = password_hash($pseudo, PASSWORD_BCRYPT, );
+
+// echo password_verify($pseudo , $hash);
 
 
 
